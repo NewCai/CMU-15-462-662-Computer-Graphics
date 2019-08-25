8b319cd125aad6add228594363c053e47c6ddb2f
Zeyong <zyc@dreamcraft.com>
2019-08-24 18:18:48 -0700

    Adjust format

--- Assignment2&3&4-Scotty3D/src/bvh.cpp
+++ Assignment2&3&4-Scotty3D/src/bvh.cpp
@@ -83,13 +83,13 @@ void BVHNode::Partition(std::vector<Primitive*>& _primitives, size_t max_leaf_si
 
 	const int BUCKET_SIZE = 8;
 
-	vector<Primitive*> partitionA, partitionB;
+	vector<Primitive *> partitionA, partitionB;
 	double sah = INF_D;
 
 	for (size_t d = 0; d < 3; ++d)
 	{
-		vector<vector<Primitive*>> buckets(BUCKET_SIZE, vector<Primitive*>());
-    vector<BBox> bucketBBoxes(BUCKET_SIZE);
+		vector<vector<Primitive *>> buckets(BUCKET_SIZE, vector<Primitive *>());
+		vector<BBox> bucketBBoxes(BUCKET_SIZE);
 
 		auto left = bb.min[d], right = bb.max[d];
 		auto space = (right - left) / BUCKET_SIZE;
@@ -99,17 +99,17 @@ void BVHNode::Partition(std::vector<Primitive*>& _primitives, size_t max_leaf_si
 			auto p = _primitives[i];
 			auto index = floorl((p->get_bbox().centroid()[d] - left) / space);
 			buckets[index].push_back(p);
-      bucketBBoxes[index].expand(p->get_bbox());
+			bucketBBoxes[index].expand(p->get_bbox());
 		}
 
-    vector<BBox> leftBBoxes(BUCKET_SIZE);
-    vector<BBox> rightBBoxes(BUCKET_SIZE);
-    for (size_t i = 1; i <= BUCKET_SIZE - 1; ++i)
+		vector<BBox> leftBBoxes(BUCKET_SIZE);
+		vector<BBox> rightBBoxes(BUCKET_SIZE);
+		for (size_t i = 1; i <= BUCKET_SIZE - 1; ++i)
 		{
-      leftBBoxes[i] = leftBBoxes[i - 1];
-      rightBBoxes[i] = rightBBoxes[i - 1];
-      leftBBoxes[i].expand(bucketBBoxes[i - 1]);
-      rightBBoxes[i].expand(bucketBBoxes[BUCKET_SIZE - i]);
+			leftBBoxes[i] = leftBBoxes[i - 1];
+			rightBBoxes[i] = rightBBoxes[i - 1];
+			leftBBoxes[i].expand(bucketBBoxes[i - 1]);
+			rightBBoxes[i].expand(bucketBBoxes[BUCKET_SIZE - i]);
 		}
 
 		size_t leftPartitionNum = 0, rightPartitionNum = 0;
@@ -152,34 +152,37 @@ void BVHNode::Partition(std::vector<Primitive*>& _primitives, size_t max_leaf_si
 		_primitives[pIndex++] = p;
 	}
 
-  if (partitionA.size() == range || partitionB.size() == range) {
-    if (partitionB.size() == range)
-    {
-      swap(partitionA, partitionB);
-    }
-    auto half = partitionA.begin() + range / 2;
-    partitionB.insert(partitionB.end(),
-                        std::make_move_iterator(half),
-                        std::make_move_iterator(partitionA.end()));
-    partitionA.erase(half, partitionA.end());
-	}
-  
+	if (partitionA.size() == range || partitionB.size() == range)
 	{
-		BBox bb;
-		for (size_t i = 0; i < partitionA.size(); ++i) {
-			bb.expand(partitionA[i]->get_bbox());
+		if (partitionB.size() == range)
+		{
+			swap(partitionA, partitionB);
 		}
-		l = new BVHNode(bb, start, partitionA.size());
-	  l->Partition(_primitives, max_leaf_size);
+		auto half = partitionA.begin() + range / 2;
+		partitionB.insert(partitionB.end(),
+						  std::make_move_iterator(half),
+						  std::make_move_iterator(partitionA.end()));
+		partitionA.erase(half, partitionA.end());
 	}
 
 	{
 		BBox bb;
-		for (size_t i = 0; i < partitionB.size(); ++i) {
+		for (size_t i = 0; i < partitionA.size(); ++i)
+		{
+			bb.expand(partitionA[i]->get_bbox());
+		}
+		l = new BVHNode(bb, start, partitionA.size());
+		l->Partition(_primitives, max_leaf_size);
+	}
+
+	{
+		BBox bb;
+		for (size_t i = 0; i < partitionB.size(); ++i)
+		{
 			bb.expand(partitionB[i]->get_bbox());
 		}
 		r = new BVHNode(bb, start + partitionA.size(), partitionB.size());
-	  r->Partition(_primitives, max_leaf_size);
+		r->Partition(_primitives, max_leaf_size);
 	}
 }
 
